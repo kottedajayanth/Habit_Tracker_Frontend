6ade44d34a1119c5af40cc336ef612160ab76158
Kotteda Jayanth <kottedajayanth45@gmail.com>
2026-02-05 10:01:22 +0530

    Integrated habit ui into APP component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import UserList from "./components/UserList";
+import HabitList from "./components/HabitList";
+import AddHabit from "./components/AddHabit";
 
 function App() {
+
+  const habitListRef = useRef();
+
+  const refreshHabits = () => {
+    habitListRef.current && habitListRef.current();
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Habit Tracker Application</h1>
+
       <UserList />
+
+      <hr />
+
+      <AddHabit refresh={refreshHabits} />
+
+      <HabitList ref={habitListRef} />
     </div>
   );
 }
